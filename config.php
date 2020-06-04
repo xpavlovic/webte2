bbd511b840d635252151ee38e41730e414d43039
xpavlovic <daky98@gmail.com>
2020-06-04 21:59:34 +0200

    udaje pre

--- config.php
+++ config.php
@@ -7,3 +7,7 @@ $password = "pedofilii_zvijat";
 $dbname = "zaverecny_projekt";
 $api_key = "99cf0f8b-8b17-4a1b-93e7-be2efaec965e";
 
+
+//moje udaje pre databazu:
+//$username = "xpavlovic";
+// $password = "aqZS5hjuzxSQ";
