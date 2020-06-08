0f592c6a79e0b337aef50844a02d320f8492003a
Igor <sranka.igor@gmail.com>
2020-06-08 22:55:33 +0200

    Pridal som statistiku a logovanie pre testovanie na masteri

--- api.php
+++ api.php
@@ -1,6 +1,36 @@
 <?php
 include 'config.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+function updateStats($script_name, $mysql)
+{
+    if ($script_name == "gulicka" || $script_name == "kyvadlo" || $script_name == "lietadlo" || $script_name == "tlmenie")
+    {
+        $query = "UPDATE stats SET $script_name = $script_name +1";
+        mysqli_query($mysql,$query);
+    }
+}
+function updateLogs($mysql, $command, $result)
+{
+    $error = 0;
+    if(strstr($result,"error") === false && strstr($result,"warning") === false)
+    {
+        $error = 1;
+    }
+    $date = date("Y-m-d H:i:s");
+    $preparedQuery = $mysql->prepare("INSERT INTO log (datetime,request,is_error,error_description) VALUES (?,?,?,?)");
+    if($error == 1)
+    {
+        $preparedQuery->bind_param($date,$command,$error,$result);
+    }
+    else
+    {
+        $preparedQuery->bind_param($date,$command,$error,null);
+    }
+    $preparedQuery->execute();
+}
+$mysql = new mysqli($servername, $username, $password,$dbname);
 if (isset($_GET['search'])) {
     if ($_GET['search'] == "scripts"){
         if ($_GET['key'] == $api_key) {
@@ -11,10 +41,11 @@ if (isset($_GET['search'])) {
                 //prikaz pre terminal
 
                 $cmd = "octave --eval 'r = $parameter;' --eval '$script_name'";
+                updateStats($mysql,$script_name );
                 $output = exec($cmd, $op, $rv);
                 //data z octave scriptu
                 $stringJSON = '{"data":[{"t":' . $op[0] . ', "y":' . $op[1] . ', "x":' . $op[2] . '},{"t":' . $op[3] . ', "y":' . $op[4] . ', "x":' . $op[5] . '}]}';
-
+                updateLogs($mysql,$cmd,$op[0]);
                 echo $stringJSON;
             } elseif (empty($_GET['parameter']) && !empty($_GET['scripts'])) {
                 $script_name = $_GET['scripts'];
@@ -24,6 +55,8 @@ if (isset($_GET['search'])) {
 
                 //data z octave scriptu
                 echo $op[0];
+                updateLogs($mysql,$script_name,$op[0]);
+
                 /*
                 foreach($op as $line){
                     echo $line . "\n";
