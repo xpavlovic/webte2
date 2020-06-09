8822a5013803065932d3dd86af06b063f7533173
xpavlovic <daky98@gmail.com>
2020-06-09 23:49:28 +0200

    tlmenie

--- dampening.php
+++ dampening.php
@@ -16,6 +16,7 @@ $_SESSION['current_page'] = 'dampening.php';
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
     <script src="script.js"></script>
+    <script src="fabric.min.js"></script>
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
@@ -34,7 +35,7 @@ $_SESSION['current_page'] = 'dampening.php';
             <label for = speed style="display: none">Rychlosť zobrazovania grafu:</label>
             <input id ='speed' type="number" name="speed" value= 0 style="display: none">
             <label for="param"><?php if (isset($input_text)) echo $input_text; ?></label>
-            <input class="form-control" id='param' type="number" name="param" value=25 >
+            <input class="form-control" id='param' type="number" name="param" value=1 >
         </div>
 
         <button id="showGraph" type="button" name="showGraph" class="btn btn-secondary float-right"><?php if (isset($submit_button)) echo $submit_button; ?></button>
@@ -52,8 +53,49 @@ $_SESSION['current_page'] = 'dampening.php';
         </div>
     </div>
 </div>
-<!--<input type="button" id = "showGraph" name="showGraph" value="GRAFUJ!">-->
+<div class="d-flex justify-content-center">
+    <div id="animation" class="col-lg-10" style="display: none">
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1400 650" style="enable-background:new 0 0 1400 650;" xml:space="preserve">
+        <style type="text/css">
+            .st0 {
+                fill: #FFFEDC;
+                stroke: #000000;
+                stroke-miterlimit: 10;
+            }
+
+            .st1 {
+                fill: #BABABA;
+                stroke: #000000;
+                stroke-miterlimit: 10;
+            }
+
+            .st2 {
+                fill: #EAEAEA;
+                stroke: #000000;
+                stroke-miterlimit: 10;
+            }
+        </style>
+            <rect x="0" y="0" class="st0" width="1400" height="650"></rect>
+            <g id="dampening">
+
+                <rect x="544.7" y="291.5" class="st1" width="130" height="20" id="suspension"></rect>
+                <circle class="st2" cx="545.2" cy="320.7" r="30" id="wheel1"></circle>
+                <circle class="st2" cx="670.1" cy="320.7" r="30" id="wheel2"></circle>
+
+            </g>
+           <!-- <rect y="350" class="st3" width="1402" height="26"></rect>-->
+    </svg>
+    </div>
+</div>
+
 <script>
+
+
+    let dampening = document.getElementById('dampening');
+    let suspension = document.getElementById('suspension');
+    let wheel1 = document.getElementById('wheel1');
+    let wheel2 = document.getElementById('wheel2');
+
     let initX1 = 0;
     let initX2 = 0;
 
@@ -66,6 +108,7 @@ $_SESSION['current_page'] = 'dampening.php';
             initX2 = data.data[0].x[(data.data[0].x.length)-1];
             initX1 = data.data[0].y[(data.data[0].y.length)-1];
             displayGraph(data.data);
+            animate_dampening(data.data);
         });
 
     });
@@ -89,10 +132,31 @@ $_SESSION['current_page'] = 'dampening.php';
                 });
         });
 
-</script>
-<div id="animationDampening">
 
-</div>
+    function move_dampening(positionCar, positionWheel) {
+        let degree_angle = positionWheel * (180/Math.PI);
+        let rotation = parseFloat(degree_angle);
+        offset = 20*(positionCar);
+        wheel1.setAttribute('transform', 'rotate(' + rotation + '544.7 291.5)');
+        wheel2.setAttribute('transform', 'rotate(' + rotation + '544.7 291.5)');
+        //suspension.setAttribute('transform', 'rotate(' + rotation + '544.7 291.5)');
+
+        dampening.setAttribute('transform', 'translate('+offset+')');
+
+    }
+
+    async function animate_dampening(data) {
+
+        var speed = Number(document.getElementById("speed").value);
+        for (var i = 0; i < data[0].x.length; i++) {
+            move_dampening(data[0].y[i],data[0].x[i]);
+
+            await sleep(speed * 100);
+        }
+
+    }
+</script>
+
 <div id = 'graph' style="margin-top: 5%"></div>
 
 </body>
