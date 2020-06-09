4e3a895618c309ed35ce4e6f875c501d1fb577f8
Michal Vrabec <michal.vrabec123@gmail.com>
2020-06-09 19:37:13 +0200

    pomenovanie grafov pri kyvadle

--- pendulum.php
+++ pendulum.php
@@ -117,7 +117,7 @@ $_SESSION['current_page'] = 'pendulum.php';
                 var parameterURL = encodeURIComponent(param + ";initUhol=" + initUhol + ";initPozicia=" + initPozicia + ";");
                 var url = "https://147.175.121.210:4629/final_p/api/scripts?scripts=kyvadlo&key=99cf0f8b-8b17-4a1b-93e7-be2efaec965e&parameter=" + parameterURL;
                 $.getJSON(url, function (data) {
-                    displayGraph(data.data);
+                    displayGraphPendulum(data.data);
                     animate_pendulum(data.data);
                     initUhol = data.data[0].x[(data.data[0].x.length)-1];
                     initPozicia = data.data[0].y[(data.data[0].y.length)-1];
@@ -153,9 +153,42 @@ $_SESSION['current_page'] = 'pendulum.php';
         var speed = Number(document.getElementById("speed").value);
         for (var i = 0; i < data[0].x.length; i++) {
             move_pendulum(data[0].y[i],data[0].x[i]);
-
             await sleep(speed * 100);
         }
+    }
+
+    function drawGraphPendulum(data,maxT,xVals,yVals)
+    {
+        var graph1 = { x: data.t, y: data.x, type: 'scatter', name: 'Angle', line: {color: '#b3a50e', width: 3 } };
+        var graph2 = { x: data.t, y: data.y, xaxis: 'x2', yaxis: 'y2', type: 'scatter', name: 'Position', line: {color: 'gray', width: 3 }  };
+        var layoutGraph = {
+            xaxis: {range : [0,maxT]},
+            yaxis: {range: [xVals.min - 0.1 ,xVals.max * 1.1]},
+            xaxis2: {range : [0,maxT]},
+            yaxis2: {range: [yVals.min - 0.1 ,yVals.max * 1.1], anchor: 'x2'},
+            grid: {rows: 1, columns: 2, pattern: 'independent'},
+            margin: {l : 150, r: 200}
+        };
+        var drawGraph = [graph1,graph2];
+        Plotly.newPlot('graph',drawGraph,layoutGraph);
+    }
+
+    async function displayGraphPendulum(data) {
+        var speed = Number(document.getElementById("speed").value);
+        var currentData = {x: [], y: [], t: []};
+        var maxT = data[0].t[data[0].t.length - 1];
+
+        yVals = {min: Math.min.apply(Math,data[0].y), max: Math.max.apply(Math,data[0].y)};
+        xVals = {min: Math.min.apply(Math,data[0].x), max: Math.max.apply(Math,data[0].x)};
+
+        for (var i = 0; i < data[0].x.length; i++) {
+            currentData.x[i] = data[0].x[i];
+            currentData.y[i] = data[0].y[i];
+            currentData.t[i] = data[0].t[i];
+            drawGraphPendulum(currentData, maxT, xVals, yVals);
+            await sleep(speed * 100);
+        }
+        document.getElementById("showGraph").disabled = false;
     }
 </script>
 </body>
